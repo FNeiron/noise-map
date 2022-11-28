28602a468740a9e931c4c9db1e5f76af6138e491
FNeiron <fazylov.2002@mail.ru>
2022-11-28 14:22:52 +0400

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function init() {
-    var data = ymaps.geoXml.load("https://disk.yandex.ru/d/e-BDI6fBL4BX1g");
+    var data = ymaps.geoXml.load("https://raw.githubusercontent.com/FNeiron/map-noise/e932336062558228cd92b215dad834c1779b0430/data.kml?token=GHSAT0AAAAAAB3HMHI45SDMF5QHAQDEGATQY4ERLQQ");
     let map = new ymaps.Map('map', {
         center: [53.23668610165901,50.203182910530764],
         zoom: 15
@@ -9,8 +9,16 @@ function init() {
         // Добавление объектов на карту.
         map.geoObjects.add(res.geoObjects);
     });
+
+    circle = new ymaps.Circle([[53.23668610165901,50.203182910530764], 100], null, { draggable: true });
+    circle.events.add('drag', function () {
+        // Объекты, попадающие в круг, будут становиться красными.
+        var objectsInsideCircle = objects.searchIntersect(circle);
+        objectsInsideCircle.setOptions({fillColor: '#ff001a'});
+        // Оставшиеся объекты - синими.
+        objects.remove(objectsInsideCircle).setOptions({fillColor: '#0081ff'});
+    });
+    map.geoObjects.add(circle);
 }
-
-
 
 ymaps.ready(init)
